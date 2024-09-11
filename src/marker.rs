62a16258c290b7ba71ab83f88096a12740a4ec83
strCarne <semionvoevoda@gmail.com>
2024-09-11 20:26:57 +0300

    feat: introduce GraphType trait for graph's type checks on compile time

---
+++ src/marker.rs
@@ -0,0 +1,17 @@
+pub trait GraphType {
+    fn type_name() -> &'static str;
+}
+
+pub struct Directed;
+impl GraphType for Directed {
+    fn type_name() -> &'static str {
+        "Directed"
+    }
+}
+
+pub struct Undirected;
+impl GraphType for Undirected {
+    fn type_name() -> &'static str {
+        "Undirected"
+    }
+}
