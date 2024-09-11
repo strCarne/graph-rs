0343e8902d971a4349a9deec0560f738ecd643eb
strCarne <semionvoevoda@gmail.com>
2024-09-11 22:47:24 +0300

    feat: implement basic operation for Graph

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -30,6 +30,50 @@ where
             _type: PhantomData,
         }
     }
+
+    pub fn insert(&mut self, key: Key, value: Value) -> Option<Value> {
+        self.insert_vertex(Vertex::new(key, value)).map(|v| v.value)
+    }
+
+    pub fn get(&self, key: &Key) -> Option<&Value> {
+        self.get_vertex(key).map(|v| &v.value)
+    }
+
+    pub fn get_mut(&mut self, key: &Key) -> Option<&mut Value> {
+        self.get_vertex_mut(key).map(|v| &mut v.value)
+    }
+
+    pub fn remove(&mut self, key: &Key) -> Option<Value> {
+        self.remove_vertex(key).map(|v| v.value)
+    }
+
+    pub fn clear(&mut self) {
+        self.vertices.clear();
+    }
+
+    pub fn len(&self) -> usize {
+        self.vertices.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.vertices.is_empty()
+    }
+
+    pub fn insert_vertex(&mut self, vertex: Vertex<Key, Value>) -> Option<Vertex<Key, Value>> {
+        self.vertices.insert(vertex.key.clone(), vertex)
+    }
+
+    pub fn get_vertex(&self, key: &Key) -> Option<&Vertex<Key, Value>> {
+        self.vertices.get(key)
+    }
+
+    pub fn get_vertex_mut(&mut self, key: &Key) -> Option<&mut Vertex<Key, Value>> {
+        self.vertices.get_mut(key)
+    }
+
+    pub fn remove_vertex(&mut self, key: &Key) -> Option<Vertex<Key, Value>> {
+        self.vertices.remove(key)
+    }
 }
 
 impl<Key, Value, Type> Default for Graph<Key, Value, Type>
