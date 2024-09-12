6859d8655a58385817da6e3c6ebafcd56ecdcb01
strCarne <semionvoevoda@gmail.com>
2024-09-12 12:35:06 +0300

    feat: implement PartialEq for Edge

--- src/edge.rs
+++ src/edge.rs
@@ -1,15 +1,34 @@
-pub struct Edge<Key> {
+pub struct Edge<Key>
+where
+    Key: Eq,
+{
     pub from: Key,
     pub to: Key,
     pub weight: i64,
 }
 
-impl<Key> Edge<Key> {
+impl<Key> Edge<Key>
+where
+    Key: Eq,
+{
     pub fn new(from: Key, to: Key, weight: i64) -> Self {
         Self { from, to, weight }
     }
 
     pub fn new_unweighted(from: Key, to: Key) -> Self {
         Self::new(from, to, 0)
+    }
+}
+
+impl<Key> PartialEq for Edge<Key>
+where
+    Key: Eq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.from == other.from && self.to == other.to
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.from != other.from || self.to != other.to
     }
 }
