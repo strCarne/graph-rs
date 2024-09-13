17c61f5c1b2c0f9fbe0d579a29282ef0afc93cf9
strCarne <semionvoevoda@gmail.com>
2024-09-13 18:17:56 +0300

    feat: make iterator over all vertecies in graph

--- src/graph/general/iterators/mod.rs
+++ src/graph/general/iterators/mod.rs
@@ -1,2 +1,20 @@
+use std::hash::Hash;
+
+use crate::{graph::Graph, marker::GraphType, vertex::Vertex};
+
 pub mod bfs;
 pub mod dfs;
+
+impl<Key, Value, Type> Graph<Key, Value, Type>
+where
+    Key: Hash + Eq + Clone,
+    Type: GraphType,
+{
+    pub fn vertecies(&self) -> impl Iterator<Item = &Vertex<Key, Value>> {
+        self.vertices.iter().map(|v| v.1)
+    }
+
+    pub fn vertecies_mut(&mut self) -> impl Iterator<Item = &mut Vertex<Key, Value>> {
+        self.vertices.iter_mut().map(|v| v.1)
+    }
+}
