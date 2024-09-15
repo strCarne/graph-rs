fe66e8582a815a0482bbe0454e7524d73fe4e37a
strCarne <semionvoevoda@gmail.com>
2024-09-15 21:07:06 +0300

    docs: add documentation for Vertex model

--- src/vertex.rs
+++ src/vertex.rs
@@ -1,11 +1,18 @@
 use crate::edge::Edge;
 
+/// Vertex is a container that represents a node in a graph.
+/// It contains a key and a value, and a list of edges that connect to it.
 pub struct Vertex<Key, Value>
 where
     Key: Eq,
 {
+    /// Key is used to identify the vertex in the graph.
     key: Key,
+
+    /// Value is used to store additional information about the vertex.
     pub value: Value,
+
+    /// Adjancency list is a list of edges that connect to the vertex.
     adj: Vec<Edge<Key>>,
 }
 
@@ -21,6 +28,9 @@ where
         }
     }
 
+    /// Inserts a new edge into the vertex's adjacency list.
+    /// Returns the old edge if it existed in the list.
+    /// Returns None if the edge did not exist in the list.
     pub fn insert_edge(&mut self, new_edge: Edge<Key>) -> Option<Edge<Key>> {
         let result = 'a: {
             for (i, edge) in self.adj.iter().enumerate() {
@@ -45,6 +55,7 @@ where
         &self.adj
     }
 
+    /// Returns the edge if it exists in the adjacency list, otherwise returns None.
     pub fn get_edge(&self, to: &Key) -> Option<&Edge<Key>> {
         for edge in &self.adj {
             if *edge.to() == *to {
@@ -54,6 +65,7 @@ where
         None
     }
 
+    /// Removes the edge if it exists in the adjacency list, otherwise returns None.
     pub fn remove_edge(&mut self, to: &Key) -> Option<Edge<Key>> {
         for (i, edge) in self.adj.iter().enumerate() {
             if *edge.to() == *to {
